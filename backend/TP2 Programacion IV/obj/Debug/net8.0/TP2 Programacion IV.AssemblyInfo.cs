261de53f8843fe268ed9353a9c148a14974cee5c
FrancoBruchilari <fbruchilari@gmail.com>
2025-11-10 20:01:06 -0300

    coruse deleted

--- backend/TP2 Programacion IV/obj/Debug/net8.0/TP2 Programacion IV.AssemblyInfo.cs
+++ backend/TP2 Programacion IV/obj/Debug/net8.0/TP2 Programacion IV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP2 Programacion IV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11656d872fe24dbb989b43c4e78b8968991fb213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acfdeeb8fd350f93dcb388e1f4299e26a476f06d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP2 Programacion IV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP2 Programacion IV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
